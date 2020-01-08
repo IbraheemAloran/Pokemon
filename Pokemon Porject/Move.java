716f26b341bb8badf42058e7ae55c9d71bf22fa8
wesam5244 <59580146+wesam5244@users.noreply.github.com>
2020-01-08 17:29:27 -0500

    Update Move.java

--- Pokemon Porject/Move.java
+++ Pokemon Porject/Move.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Move{
+public class Move implements Global{
 	private String name;
 	private int pp;
 	private int type;
@@ -23,13 +23,35 @@ public class Move{
 
 	public boolean useMove(Pokemon attacking, Pokemon defending){
 		if (hit()){
-
-			return true;
+			if(cat!=8){
+				int STAB = 1;
+				if (type == attacking.type) STAB = 1.5;
+				int damage = calculateDamage(STAB, attacking, defending);
+				defending.changeHP(damage * -1);
+			}
+			return true;	
 		}
 		return false;
 	}
 
-	public boolean hit(){
+	private int calculateDamage(int STAB, Pokemon attacking, Pokemon defending){
+		int damage = ((2 * attacking.getLevel()) / 5) + 2;
+		damage *= power;
+		if (cat == 6){
+			damage *= (attacking.getAttack() / defending.getDefense());
+		}
+		else if (cat == 7){
+			damage *= (attacking.getSpecialAttack() / defending.getSpecialDefense());
+		}
+		damage /= 50;
+		damage += 2;
+		damage *= STAB;
+		int multiplier = typeChart[type - 9].get(defending.getType());
+		damage *= multiplier;
+		return damage;
+		
+	}
+	private boolean hit(){
 		Random rand = new Random();
 		int choice = rand.nextInt(100);
 		if (choice <= acc) return true;
