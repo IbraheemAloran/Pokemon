454768cd5df83d83b0699a56dd7f6f7b71224df8
wesam5244 <59580146+wesam5244@users.noreply.github.com>
2020-01-08 17:29:10 -0500

    Update Global.java

--- Pokemon Porject/Global.java
+++ Pokemon Porject/Global.java
@@ -8,8 +8,8 @@ public interface Global
 	int DEFENSE = 3;
 	int SDEFENSE = 4;
 	int SPEED = 5;
-	int SPECIAL = 6;
-	int PHYSICAL = 7;
+	int SPECIAL = 7;
+	int PHYSICAL = 6;
 	int STATUS = 8;
 
 //TYPES
@@ -33,5 +33,91 @@ public interface Global
 
 //EEFECTIVENESS
 	HashMap[] typeChart = new HashMap[8];
+	c = new HashMap<Integer,Integer>();
 	typeChart[0] = new HashMap<Integer,Integer>();
+	typeChart[1] = new HashMap<Integer,Integer>();
+	typeChart[2] = new HashMap<Integer,Integer>();
+	typeChart[3] = new HashMap<Integer,Integer>();
+	typeChart[4] = new HashMap<Integer,Integer>();
+	typeChart[5] = new HashMap<Integer,Integer>();
+	typeChart[6] = new HashMap<Integer,Integer>();
+	typeChart[7] = new HashMap<Integer,Integer>();
+	
+	
+//0 RERESENTS FIRE
+	typeChart[0].put(FIRE, 0.5); 
+	typeChart[0].put(WATER, 0.5); 
+	typeChart[0].put(ELECTRIC, 1);
+	typeChart[0].put(GRASS, 2);	
+	typeChart[0].put(NORMAL, 1);
+	typeChart[0].put(DARK, 1);
+	typeChart[0].put(PSYCHIC, 1);
+	typeChart[0].put(GHOST, 1);
+	
+//1 REPRESENTS ELECTRIC
+	typeChart[1].put(ELECTRIC, 0.5);
+	typeChart[1].put(FIRE, 1); 
+	typeChart[1].put(WATER, 2);
+	typeChart[1].put(GRASS, 0.5);	
+	typeChart[1].put(NORMAL, 1);
+	typeChart[1].put(DARK, 1);
+	typeChart[1].put(PSYCHIC, 1);
+	typeChart[1].put(GHOST, 1);
+	
+//WATER
+	typeChart[2].put(WATER, 0.5);
+	typeChart[2].put(FIRE, 2);
+	typeChart[2].put(ELECTRIC, 1);	
+	typeChart[2].put(GRASS, 0.5);	
+	typeChart[2].put(NORMAL, 1);
+	typeChart[2].put(DARK, 1);
+	typeChart[2].put(PSYCHIC, 1);
+	typeChart[2].put(GHOST, 1);
+	
+//GRASS
+	typeChart[3].put(GRASS, 0.5);
+	typeChart[3].put(FIRE, 0.5);
+	typeChart[3].put(ELECTRIC, 1);	
+	typeChart[3].put(WATER, 2);	
+	typeChart[3].put(NORMAL, 1);
+	typeChart[3].put(DARK, 1);
+	typeChart[3].put(PSYCHIC, 1);
+	typeChart[3].put(GHOST, 1);
+
+//NORMAL
+	typeChart[4].put(NORMAL, 1);
+	typeChart[4].put(FIRE, 1);
+	typeChart[4].put(ELECTRIC, 1);	
+	typeChart[4].put(GRASS, 1);	
+	typeChart[4].put(WATER, 1);
+	typeChart[4].put(DARK, 1);
+	typeChart[4].put(PSYCHIC, 1);
+	typeChart[4].put(GHOST, 0);
+	
+	typeChart[5].put(DARK, 0.5);//DARK
+	typeChart[5].put(FIRE, 1);
+	typeChart[5].put(ELECTRIC, 1);	
+	typeChart[5].put(GRASS, 1);	
+	typeChart[5].put(WATER, 1);
+	typeChart[5].put(NORMAL, 1);
+	typeChart[5].put(PSYCHIC, 2);
+	typeChart[5].put(GHOST, 2);
+	
+	typeChart[6].put(PSYCHIC, 0.5);//PSYCHIC
+	typeChart[6].put(FIRE, 1);
+	typeChart[6].put(ELECTRIC, 1);	
+	typeChart[6].put(GRASS, 1);	
+	typeChart[6].put(WATER, 1);
+	typeChart[6].put(NORMAL, 1);
+	typeChart[6].put(DARK, 0);
+	typeChart[6].put(GHOST, 1);
+
+	typeChart[7].put(GHOST, 2);//GHOST
+	typeChart[7].put(FIRE, 1);
+	typeChart[7].put(ELECTRIC, 1);	
+	typeChart[7].put(GRASS, 1);	
+	typeChart[7].put(WATER, 1);
+	typeChart[7].put(NORMAL, 0);
+	typeChart[7].put(PSYCHIC, 2);
+	typeChart[7].put(DARK, 0.5);
 }
